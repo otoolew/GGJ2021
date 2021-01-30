3ac8f1395df7bc47f1256e80e2bff8fb76190504
Kati Baker <Bakenshake09@gmail.com>
2021-01-30 18:48:37 -0500

    spawn statues

--- Assets/Code/Obstacles/SpawnManager.cs
+++ Assets/Code/Obstacles/SpawnManager.cs
@@ -27,8 +27,9 @@ public class SpawnManager : MonoBehaviour
             // arbitrary spawn values - will be based on the width of the river
             for(int i=0; i<numOfObstacles;i++)
             {
-                Vector3 obstacleSpawn = new Vector3(Random.Range(spawnWidthLeft, spawnWidthRight), 0, Random.Range(-40,-80f));
+                Vector3 obstacleSpawn = new Vector3(Random.Range(spawnWidthLeft, spawnWidthRight), -3f, Random.Range(-40,-80f));
                 Instantiate(obstacle, obstacleSpawn, Quaternion.identity);
+                //obstacle.transform.Rotate(0, Random.Range(-45, 45), 0, Space.Self);
             }
             yield return new WaitForSeconds(obstacleSpawnTime);
         }
