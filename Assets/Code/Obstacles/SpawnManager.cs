a313a5aa3abdc466d8d00609e81ab3544abb835c
Kati Baker <Bakenshake09@gmail.com>
2021-01-30 17:53:30 -0500

    spawner creates enemies and obstacles
    - obstacles are rectangles
    - enemies are cylinders

--- Assets/Code/Obstacles/SpawnManager.cs
+++ Assets/Code/Obstacles/SpawnManager.cs
@@ -6,6 +6,7 @@ public class SpawnManager : MonoBehaviour
 {
 
     public GameObject obstacle;
+    public GameObject enemy;
     public float obstacleSpawnTime;
     public int numOfObstacles;
 
@@ -14,9 +15,13 @@ public class SpawnManager : MonoBehaviour
 
     public float despawnPoint;
 
+    private GameObject[] spawnableList = new GameObject[2];
+
     // Start is called before the first frame update
     private void Start()
     {
+        spawnableList[0] = obstacle;
+        spawnableList[1] = enemy;
         StartCoroutine(ObstacleSpawn());
     }
 
@@ -28,7 +33,7 @@ public class SpawnManager : MonoBehaviour
             for(int i=0; i<numOfObstacles;i++)
             {
                 Vector3 obstacleSpawn = new Vector3(Random.Range(spawnWidthLeft, spawnWidthRight), 0, Random.Range(-40,-80f));
-                Instantiate(obstacle, obstacleSpawn, Quaternion.identity);
+                Instantiate(spawnableList[Random.Range(0,2)], obstacleSpawn, Quaternion.identity);
             }
             yield return new WaitForSeconds(obstacleSpawnTime);
         }
