a639bdb842183853b4d6d2fc863e2ab3a15ccfa3
Kati Baker <Bakenshake09@gmail.com>
2021-01-30 22:40:56 -0500

    added timer on screen

--- Assets/Code/GameMode/GameMode.cs
+++ Assets/Code/GameMode/GameMode.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 [Serializable] public enum GameState { RUNNING, PAUSED, LOADING, GAME_OVER }
